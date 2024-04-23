63f255036e426f4ad91f511e8f94557e4c5eb3e8
KeiiZ07 <memelonz@outlook.com>
2024-04-23 01:29:57 -0400

    pageSeparate

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,9 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+console.log('Hello')
+
+document.getElementById('nextPageButton').onclick = function(){
+    window.location.href = 'page1.html'
+}
